08c0382d6d8a75c71b3f5bd8b1329b7fab797a5a
Tkheshong <108187896+Tkheshong@users.noreply.github.com>
2023-05-24 12:45:30 -0500

    Update PriceTransparencyNoInsTest.java

--- src/test/java/test/com/itexps/PriceTransparencyNoInsTest.java
+++ src/test/java/test/com/itexps/PriceTransparencyNoInsTest.java
@@ -41,12 +41,7 @@ public class PriceTransparencyNoInsTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        ChromeOptions options = new ChromeOptions(); 
-  options.addArguments("--headless"); 
-options.addArguments("--disable-gpu"); 
- options.addArguments("--window-size=1400,800");
- 
-WebDriver driver = new ChromeDriver(options); 
+  
     }
 
     @AfterClass
