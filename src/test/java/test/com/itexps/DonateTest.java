80332fd156f2d817fc2b13b964f5d5b29305ddd7
Tkheshong <108187896+Tkheshong@users.noreply.github.com>
2023-05-24 12:45:45 -0500

    Update DonateTest.java

--- src/test/java/test/com/itexps/DonateTest.java
+++ src/test/java/test/com/itexps/DonateTest.java
@@ -40,13 +40,7 @@ public class DonateTest {
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-        ChromeOptions options = new ChromeOptions(); 
-  options.addArguments("--headless"); 
-options.addArguments("--disable-gpu"); 
- options.addArguments("--window-size=1400,800");
- 
-WebDriver driver = new ChromeDriver(options); 
-    }
+         }
     
     @AfterClass
     public static void tearDownClass() throws Exception {
